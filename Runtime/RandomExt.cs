780a80b9ce8062059c91ca65ab2f0496a72b227e
underdogg <47274328+und3rd0gg@users.noreply.github.com>
2023-06-08 23:22:14 +0300

    v1.1.1

---
+++ Runtime/RandomExt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UniExt
+{
+    public static class RandomExt
+    {
+        public static bool NextBoolean(this Random random) =>
+            random.Next() > int.MaxValue / 2;
+    }
+}
